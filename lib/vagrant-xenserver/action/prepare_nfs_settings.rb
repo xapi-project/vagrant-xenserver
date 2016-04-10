8d39259bb9123db092592343f7aa9b7a8bd3b262
Sayid Munawar <sayid.munawar@gmail.com>
2016-04-10 10:32:23 +0700

    fix prepare_nfs_settings.rb, add ping test and rework the call() method

--- lib/vagrant-xenserver/action/prepare_nfs_settings.rb
+++ lib/vagrant-xenserver/action/prepare_nfs_settings.rb
@@ -40,68 +40,68 @@ module VagrantPlugins
           if using_nfs?
             @logger.info("Using NFS, preparing NFS settings by reading host IP and machine IP")
 
+            # Original code
             env[:nfs_host_ip]    = read_host_ip(env[:machine],env)
+            env[:nfs_machine_ip] = env[:xs_host_ip]
 
-            has_public_network = false
-            bridge = nil
-            # First, check if there is public_network defined in Vagrantfile
-            # since we don't know which interface is ablt to connect to
-            # NFS Server, assume the last one
-            env[:machine].config.vm.networks.each do |type, options|
-              next if type == :forwarded_port
+            # Get VM UUID
+            vm_result = env[:xc].call("VM.get_record",env[:session],env[:machine].id)['Value']
 
-              if type == :public_network then
-                has_public_network = true
-                bridge = options[:bridge]
+            # Get all Networks
+            networks = env[:xc].call("network.get_all_records",env[:session])
+            public_network_defined = false
+            vm_ip = nil
+
+            # check if there is public_network defined in Vagrantfile
+            # pick the one which is routable, and return
+            if networks["Status"] == "Success"
+              env[:machine].config.vm.networks.each do |type, options|
+                next if type == :forwarded_port
+
+                if type == :public_network then
+                  public_network_defined = true
+
+                  # Find which network has the bridge name match in Vagrantfile
+                  # and is routable
+                  xenbr_net = networks['Value'].find { |ref,net| net['bridge'] == options[:bridge] }
+                  (ref, xenbr_rec) = xenbr_net
+
+                  # Find the VIF's "device" number, e.g. device 2 is eth2 in a centos guest
+                  xenbr_vif = xenbr_rec['VIFs'].find { |vif| vm_result['VIFs'].include? vif }
+                  vif = env[:xc].call("VIF.get_record", env[:session], xenbr_vif)['Value']
+                  print "Finding guest IP in #{options[:bridge]}... "
+                  @logger.info("VM UUID: #{vm_result['uuid']}, #{options[:bridge]} network name-label \"#{xenbr_rec['name_label']}\"")
+
+                  # I don't know the xmlrpc call to get IP address on a VIF, assume
+                  # XenTools is installed, we can get IP from `xe vm-list` command
+                  re = /#{vif['device']}\/ip:\s+(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3});/
+                  xe = ["vm-list uuid=#{vm_result['uuid']} params=networks --minimal", env]
+                  match = Xe::XEViaSSH.new(*xe).execute.stdout.chomp.match re
+
+                  if not match.nil?
+                    vm_ip = match[1]
+                    puts vm_ip
+                    @logger.info("VM nic on #{options[:bridge]} is eth#{vif['device']} with IPv4 address: #{vm_ip}")
+                    if ping(vm_ip)
+                      # Finally, we know what to put in /etc/exports
+                      env[:nfs_machine_ip] = vm_ip
+                      # Is it common to return like this ? dunno ruby
+                      return
+                    end
+                  else
+                    # just print empty blank line :D
+                    puts
+                  end
+                end
               end
             end
 
-            if has_public_network then
-              # I don't know the xmlrpc call to get IP address on a VIF, assume
-              # XenTools is installed, we can get IP from `xe vm-list` command
+            # public_network defined, but unreachable or has no IP
+            raise Vagrant::Errors::NFSNoGuestIP if public_network_defined && vm_ip.nil?
+            # no public_network but invalid nfs_host_ip
+            raise Vagrant::Errors::NFSNoHostonlyNetwork if !env[:nfs_machine_ip] || !env[:nfs_host_ip]
 
-              # Get VM UUID
-              vm_result = env[:xc].call("VM.get_record",env[:session],env[:machine].id)['Value']
-              # Get all Networks
-              networks = env[:xc].call("network.get_all_records",env[:session])['Value']
-
-              # Find which network has the bridge name match in Vagrantfile
-              xenbr_net = networks.find { |ref,net| net['bridge'] == bridge }
-              (ref, xenbr_rec) = xenbr_net
-
-              # Find the VIF's "device" number, device 2 is eth2 in centos guest
-              xenbr_vif = xenbr_rec['VIFs'].find { |vif| vm_result['VIFs'].include? vif }
-              vif = env[:xc].call("VIF.get_record",env[:session], xenbr_vif)['Value']
-
-              vm_uuid = vm_result['uuid']
-              @logger.info("VM UUID: #{vm_uuid}, #{bridge} network name-label \"#{xenbr_rec['name_label']}\"")
-
-              # Get VM IP
-              # FIXME: stupidly append env to the end, don't know the proper way
-              #         so that xe.rb can read Vagrantfile config
-              re = /#{vif['device']}\/ip:\s+(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3});/
-              cmd = ["vm-list uuid=#{vm_result['uuid']} params=networks --minimal", env]
-              match = Xe::XEViaSSH.new(*cmd).execute.stdout.chomp.match re
-
-              if not match.nil?
-                vm_ip = match[1]
-                @logger.info("VM nic on #{bridge} is eth#{vif['device']} with IPv4 address: #{vm_ip}")
-
-                # Finally, we know what to put in /etc/exports
-                env[:nfs_machine_ip] = vm_ip
-                @logger.info("host IP: #{env[:nfs_host_ip]} machine IP: #{env[:nfs_machine_ip]}")
-
-              else
-                raise Vagrant::Errors::NFSNoGuestIP
-              end
-            else
-
-              # Original code
-              env[:nfs_machine_ip] = env[:xs_host_ip]
-              @logger.info("host IP: #{env[:nfs_host_ip]} machine IP: #{env[:nfs_machine_ip]}")
-
-              raise Vagrant::Errors::NFSNoHostonlyNetwork if !env[:nfs_machine_ip] || !env[:nfs_host_ip]
-            end
+            @logger.info("host IP: #{env[:nfs_host_ip]} machine IP: #{env[:nfs_machine_ip]}")
           end
         end
 
@@ -133,9 +133,6 @@ module VagrantPlugins
             match[1]
           end
           def get_local_ip_win(ip)
-            error = "For NFS in windows, you need to install vagrant-winnfsd plugin, and put the winnfsd executable on Vagrant's bin directory"
-            raise error unless Vagrant.has_plugin? "vagrant-winnfsd"
-
             # Assume default gateway interface has IP address which reachable from Xenserver Host
             re = /^.*0\.0\.0\.0\s+\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\s+(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}).*$/
             match = `route -4 PRINT 0.0.0.0`.match re
@@ -146,6 +143,19 @@ module VagrantPlugins
           elsif os == :windows then get_local_ip_win(ip)
           else raise Vagrant::Errors::UnknownOS # "unknown os: #{host_os.inspect}"
           end
+        end
+
+        # Check if we can open a connection to the host
+        def ping(host, timeout = 3)
+          Timeout::timeout(timeout) do
+            s = TCPSocket.new(host, 'echo')
+            s.close
+          end
+          true
+        rescue Errno::ECONNREFUSED
+          true
+        rescue Timeout::Error, StandardError
+          false
         end
       end
     end
